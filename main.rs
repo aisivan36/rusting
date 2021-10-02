bdf38498154e2f79c70276d7d130bdc662fa295b
ivan-boginski <aisivan36@gmail.com>
2021-10-02 07:37:57 +0700

    start

---
+++ main.rs
@@ -0,0 +1,22 @@
+// use std::io;
+
+fn main() {
+   // println!("Guess the number!");
+
+   // println!("Please input your guess.");
+
+   // let mut guess = String::new();
+
+   // io::stdin()
+   //    .read_line(&mut guess)
+   //    .expect("Failed to read line");
+
+   let x = 13;
+
+   let mut x = x + 12;
+   println!("You guessed: {}", x);
+
+   x += 12;
+
+   println!("You guessed: {}", x);
+}
